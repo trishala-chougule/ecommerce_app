1207d14ed0413792fddae013a40f52456a38424f
trishala.chougule <trishala.chougule@biz4soltions.com>
2025-02-27 14:35:02 +0530

    Built web
    Branch: master
    Commit: 0a8ee564fa73402f9c849f4ffb203d5e3ec36889 (dirty)
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "076e20447bde795c5342d9dc1c7ea997
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "c63e700f52fdd65b3d651ee082cf4cec",
+"flutter_bootstrap.js": "1cf305d2652be5fef469d945ad91467b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
