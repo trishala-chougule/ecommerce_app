33f0aec35c7729d52acc07beb23154b0a7414a61
trishala.chougule <trishala.chougule@biz4soltions.com>
2025-02-28 18:49:43 +0530

    Built web
    Branch: master
    Commit: e3bae7183585ed663dfe087f19f4cd7d7d60a679 (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1543637381"
+    serviceWorkerVersion: "1550806928"
   }
 });
