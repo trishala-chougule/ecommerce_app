178dd89328b1c8224f45355d59ecc566b59fe909
trishala.chougule <trishala.chougule@biz4soltions.com>
2025-02-28 11:44:13 +0530

    Built web
    Branch: master
    Commit: e3bae7183585ed663dfe087f19f4cd7d7d60a679 (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "842331274"
+    serviceWorkerVersion: "2933449340"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "076e20447bde795c5342d9dc1c7ea997
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "d346fc0817e51d253f162fde9522f8f2",
+"flutter_bootstrap.js": "a6e21774a13ffee6784475c611e470dc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
